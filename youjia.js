9f840aed514c6fdb652d5d48fad36398ff41f742
chaneldiorrossi <23490400+laperlarossi@users.noreply.github.com>
2024-08-31 09:40:54 +0800

    Update youjia.js

--- youjia.js
+++ youjia.js
@@ -1,28 +1,15 @@
-#!name=实时油价模块
-#!desc=显示本省实时油价
-/*
-[Panel]
-YJ = script-name=YJ,update-interval=43200
-[Script]
-YJ = type=generic,timeout=5,script-path=https://raw.githubusercontent.com/laperlarossi/Panel/main/youjia.js,argument=shanghai
-*/
-
-var region = 'shanxi-3/xian'
-
+// 指定查询地区，可通过argument或persistentStore设置，后者优先级高
+var region = 'hainan';
 if (typeof $argument !== 'undefined' && $argument !== '') {
-    region = $argument
+    region = $argument;
 }
 
-try{
-//持久化适合远程引用不添加本地模块
-//工具>脚本编辑器>左下角齿轮图标>$persistentStore
-const region_pref = $persistentStore.read("yj");
-	if (typeof region_pref !== 'undefined' && region_pref !== null) { //Surge Loon写法
-		console.log("2")
-    region = region_pref
-}}catch(i){}
+const region_pref = $persistentStore.read("gas_price_region");
+if (typeof region_pref !== 'undefined' && region_pref !== '') {
+    region = region_pref;
+}
 
-const query_addr = `http://m.qiyoujiage.com/${region}.shtml`
+const query_addr = `http://m.qiyoujiage.com/${region}.shtml`;
 
 $httpClient.get(
     {
@@ -33,13 +20,13 @@ $httpClient.get(
         },
     }, (error, response, data) => {
         if (error) {
-            console.log(`解析油价信息失败, 请反馈至 @RS0485: URL=${query_addr}`)
+            console.log(`解析油价信息失败, 请反馈至 @RS0485: URL=${query_addr}`);
             done({});
         }
         else {
-            const reg_price = /<dl>[\s\S]+?<dt>(.*油)<\/dt>[\s\S]+?<dd>(.*)\(元\)<\/dd>/gm
+            const reg_price = /<dl>[\s\S]+?<dt>(.*油)<\/dt>[\s\S]+?<dd>(.*)\(元\)<\/dd>/gm;
 
-            var prices = []
+            var prices = [];
             var m = null;
 
             while ((m = reg_price.exec(data)) !== null) {
@@ -51,51 +38,51 @@ $httpClient.get(
                 prices.push({
                     name: m[1],
                     value: `${m[2]} 元/L`
-                })
+                });
             }
 
             // 解析油价调整趋势
-            var adjust_date = ''
-            var adjust_trend = ''
-            var adjust_value = ''
+            var adjust_date = '';
+            var adjust_trend = '';
+            var adjust_value = '';
 
-            const reg_adjust_tips = /<div class="tishi"> <span>(.*)<\/span><br\/>([\s\S]+?)<br\/>/
-            const adjust_tips_match = data.match(reg_adjust_tips)
+            const reg_adjust_tips = /<div class="tishi"> <span>(.*)<\/span><br\/>([\s\S]+?)<br\/>/;
+            const adjust_tips_match = data.match(reg_adjust_tips);
 
             if (adjust_tips_match && adjust_tips_match.length === 3) {
-                adjust_date = adjust_tips_match[1].split('价')[1].slice(0, -2)
+                adjust_date = adjust_tips_match[1].split('价')[1].slice(0, -2);
 
-                adjust_value = adjust_tips_match[2]
-                adjust_trend = (adjust_value.indexOf('下调') > -1 || adjust_value.indexOf('下跌') > -1) ? '↓' : '↑'
+                adjust_value = adjust_tips_match[2];
+                adjust_trend = (adjust_value.indexOf('下调') > -1 || adjust_value.indexOf('下跌') > -1) ? '↓' : '↑';
 
-                const adjust_value_re = /([\d\.]+)元\/升-([\d\.]+)元\/升/
-                const adjust_value_re2 = /[\d\.]+元\/吨/
-                const adjust_value_match = adjust_value.match(adjust_value_re)
+                const adjust_value_re = /([\d\.]+)元\/升-([\d\.]+)元\/升/;
+                const adjust_value_re2 = /[\d\.]+元\/吨/;
+                const adjust_value_match = adjust_value.match(adjust_value_re);
 
                 if (adjust_value_match && adjust_value_match.length === 3) {
-                    adjust_value = `${adjust_value_match[1]}-${adjust_value_match[2]}元/L`
+                    adjust_value = `${adjust_value_match[1]}-${adjust_value_match[2]}元/L`;
                 }
                 else {
-                    const adjust_value_match2 = adjust_value.match(adjust_value_re2)
+                    const adjust_value_match2 = adjust_value.match(adjust_value_re2);
 
                     if (adjust_value_match2) {
-                        adjust_value = adjust_value_match2[0]
+                        adjust_value = adjust_value_match2[0];
                     }
                 }
             }
 
-            const friendly_tips = `${adjust_date} ${adjust_trend} ${adjust_value}`
+            const friendly_tips = `${adjust_date} ${adjust_trend} ${adjust_value}`;
 
             if (prices.length !== 4) {
-                console.log(`解析油价信息失败, 数量=${prices.length}, 请反馈至 @RS0485: URL=${query_addr}`)
-                done({})
+                console.log(`解析油价信息失败, 数量=${prices.length}, 请反馈至 @RS0485: URL=${query_addr}`);
+                done({});
             }
             else {
                 body = {
                     title: "实时油价信息",
                     content: `${prices[0].name}  ${prices[0].value}\n${prices[1].name}  ${prices[1].value}\n${prices[2].name}  ${prices[2].value}\n${prices[3].name}  ${prices[3].value}\n${friendly_tips}`,
                     icon: "fuelpump.fill"
-                }
+                };
 
                 $done(body);
             }
